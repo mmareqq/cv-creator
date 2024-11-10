6a81f96f7ff461601deec8027e41d5a70e7d6e30
mmareqq <szlejter.marek007@gmail.com>
2024-11-10 15:04:38 +0100

    Initial commit, initiial configuration

---
+++ README.md
@@ -0,0 +1,3 @@
+# CV Creator
+
+CV Creator app project created using JS React to practice front-end skills with The Odin Project Cirruculum.
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './styles/reset.css';
+import './styles/global.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello, world!</h1>
+    </>
+  );
+}
+
+export default App;
